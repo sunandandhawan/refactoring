be5e1da9084a242ac0d4aeee49b5f6c72e71329c
Sunandan Dhawan <sunandan94@gmail.com>
2021-05-06 09:31:26 +0530

    Remove `play` parameter from `amountFor` function (Change Function Declaration)

--- statement.js
+++ statement.js
@@ -8,7 +8,7 @@ module.exports.statement = function (invoice, plays) {
     minimumFractionDigits: 2,
   }).format;
   for (let perf of invoice.performances) {
-    let thisAmount = amountFor(perf, playFor(perf));
+    let thisAmount = amountFor(perf);
     // add volume credits
     volumeCredits += Math.max(perf.audience - 30, 0);
     // add extra credit for every ten comedy attendees
@@ -28,9 +28,9 @@ module.exports.statement = function (invoice, plays) {
     return plays[aPerformance.playID];
   }
 
-  function amountFor(aPerformance, play) {
+  function amountFor(aPerformance) {
     let result = 0;
-    switch (play.type) {
+    switch (playFor(aPerformance).type) {
       case "tragedy":
         result = 40000;
         if (aPerformance.audience > 30) {
@@ -45,7 +45,7 @@ module.exports.statement = function (invoice, plays) {
         result += 300 * aPerformance.audience;
         break;
       default:
-        throw new Error(`unknown type: ${play.type}`);
+        throw new Error(`unknown type: ${playFor(aPerformance).type}`);
     }
     return result;
   }
