a053801d794827525c1ef46d4e48867947ec0c3d
Sunandan Dhawan <sunandan94@gmail.com>
2021-05-07 04:45:35 +0530

    To display money as a penny­integer number, we need a decimal, so formatting function should take care of the division.

--- statement.js
+++ statement.js
@@ -5,12 +5,12 @@ module.exports.statement = function (invoice, plays) {
   for (let perf of invoice.performances) {
     volumeCredits += vaolumeCreditsFor(perf);
     // print line for this order
-    result += ` ${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${
+    result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
     } seats)\n`;
     totalAmount += amountFor(perf);
   }
-  result += `Amount owed is ${usd(totalAmount / 100)}\n`;
+  result += `Amount owed is ${usd(totalAmount)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return result;
 
@@ -53,6 +53,6 @@ module.exports.statement = function (invoice, plays) {
       style: "currency",
       currency: "USD",
       minimumFractionDigits: 2,
-    }).format(aNumber);
+    }).format(aNumber / 100);
   }
 };
