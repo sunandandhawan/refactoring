8e6d425749e5e24f630a5075aa734eca2fa6d2cc
Sunandan Dhawan <sunandan94@gmail.com>
2021-05-07 06:52:00 +0530

    Add function for htmlStatement and add test for it

--- statement.spec.js
+++ statement.spec.js
@@ -1,5 +1,5 @@
 const { expect } = require("chai");
-const { statement } = require("./statement");
+const { statement, htmlStatement } = require("./statement");
 describe("statement", () => {
   const plays = {
     hamlet: { name: "Hamlet", type: "tragedy" },
@@ -23,8 +23,14 @@ describe("statement", () => {
       },
     ],
   };
-  it("should return correct output", () => {
+  it("statement should return correct output", () => {
     const expected = `Statement for BigCo\n Hamlet: $650.00 (55 seats)\n As You Like It: $580.00 (35 seats)\n Othello: $500.00 (40 seats)\nAmount owed is $1,730.00\nYou earned 47 credits\n`;
     expect(statement(invoice, plays)).equals(expected);
+  });
+
+  it("htmlStatement should return correct output", () => {
+    const expected = `<h1>Statement for BigCo</h1>\n<table>\n  <tr><th>play</th><th>seats</th><th>cost</th></tr>\n  <tr><td>[object Object]</td><td>55</td><td>65000</td></tr>\n  <tr><td>[object Object]</td><td>35</td><td>58000</td></tr>\n  <tr><td>[object Object]</td><td>40</td><td>50000</td></tr>\n</table>\n<p>Amount owed is <em>$1,730.00</em></p>\n<p>You earned <em>47</em> credits</p>\n`;
+    console.log(htmlStatement(invoice, plays));
+    expect(htmlStatement(invoice, plays)).equals(expected);
   });
 });
