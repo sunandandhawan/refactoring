0d1f8519da01f78ba77fb2aeec2d311f55b60f4f
Sunandan Dhawan <sunandan94@gmail.com>
2021-05-23 17:07:54 +0530

    Move custom volumeCredits calculation of comedy from PerformanceCalculator to ComedyCalculator

--- comedyCalculator.js
+++ comedyCalculator.js
@@ -9,4 +9,8 @@ module.exports = class ComedyCalculator extends PerformanceCalculator {
     result += 300 * this.performance.audience;
     return result;
   }
+
+  get volumeCredits() {
+    return super.volumeCredits + Math.floor(this.performance.audience / 5);
+  }
 };
--- performanceCalculator.js
+++ performanceCalculator.js
@@ -9,10 +9,6 @@ module.exports = class PerformanceCalculator {
   }
 
   get volumeCredits() {
-    let result = 0;
-    result += Math.max(this.performance.audience - 30, 0);
-    if ("comedy" === this.play.type)
-      result += Math.floor(this.performance.audience / 5);
-    return result;
+    return Math.max(this.performance.audience - 30, 0);
   }
 };
